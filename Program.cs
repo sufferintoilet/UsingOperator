0b739bf65272bf786c1baf658796327b3714524b
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-07 00:33:38 +0900

    1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+namespace UsingOperator 
+{ 
+class Program 
+     { private static void Main(string[] args) 
+
+    { 
+    float width; float height; Console.Write("Enter width: 4"); 
+    width = float.Parse(Console.ReadLine()); 
+    Console.Write("Enter height: 5"); 
+    height = float.Parse(Console.ReadLine()); 
+    float area =  width * height;
+    Console.WriteLine("Area is: " + area); 
+    } 
+ } 
+ 
+ }
